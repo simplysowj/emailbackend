ba2ccf290d499f64f1d7e20d7c4b8207ae7a775d
sowjanya <90475451+simplysowj@users.noreply.github.com>
2025-04-09 18:27:00 -0400

    Update settings.py

--- email_automation/settings.py
+++ email_automation/settings.py
@@ -72,6 +72,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
 ]
+MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:8000",
@@ -160,3 +161,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'  
 STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
